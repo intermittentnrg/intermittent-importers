f73fed22bb9a5092a77e10a0e3616cc2b09d6882
intermittent.energy <git@intermittent.energy>
2021-12-01 17:31:08 +0000

    fingrid frequency load with batch size

--- scripts/fingrid-frequency-load.rb
+++ scripts/fingrid-frequency-load.rb
@@ -6,8 +6,26 @@ require 'fastest-csv'
 
 require 'pp'
 
+BATCH_SIZE = 10_000
+
 require 'influxdb'
-influxdb = InfluxDB::Client.new url: ENV['INFLUXDB_URL'], time_precision: "ms", async: true
+async_options = {
+  # number of points to write to the server at once
+  max_post_points:     BATCH_SIZE,
+  # queue capacity
+  max_queue_size:      BATCH_SIZE,
+  # number of threads
+  num_worker_threads:  1,
+  # max. time (in seconds) a thread sleeps before
+  # checking if there are new jobs in the queue
+  sleep_interval:      1,
+  # whether client will block if queue is full
+  block_on_full_queue: true,
+  # Max time (in seconds) the main thread will wait for worker threads to stop
+  # on shutdown. Defaults to 2x sleep_interval.
+  shutdown_timeout: 10
+}
+influxdb = InfluxDB::Client.new url: ENV['INFLUXDB_URL'], time_precision: "ms", async: async_options
 
 influxdb.create_retention_policy('1d', 'intermittency', '0s', '1', false, shard_duration: '1d')
 
@@ -20,7 +38,7 @@ ARGV.each do |file|
   $stderr.write "\n#{file}"
   FastestCSV.open(file) do |csv|
     csv.shift
-    csv.each_slice(1000) do |slice|
+    csv.each_slice(BATCH_SIZE) do |slice|
       data = slice.map do |row|
         time = InfluxDB.convert_timestamp(DateTime.parse(row[0]).to_time, 'ms')
         {
