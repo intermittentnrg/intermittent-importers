dc1ee9c2f5065df59845009e3b694e9dc1b0f4a8
intermittent.energy <git@intermittent.energy>
2021-12-06 02:05:20 +0000

    Run tests when ENTSOE_TOKEN is not set

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'vcr'
 require './lib/entsoe'
 require './lib/svk'
 
+ENV['ENTSOE_TOKEN'] ||= 'DUMMYTOKEN'
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
