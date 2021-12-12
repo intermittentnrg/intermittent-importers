51b7da0d12d2b8f9f0b9b5bef9b16785ed82afdb
intermittent.energy <git@intermittent.energy>
2021-12-12 19:14:05 +0000

    Run jenkins cron job every 4 hours

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -1,21 +1,19 @@
-['intermittency-entsoe'].each { jobName ->
-  pipelineJob(jobName) {
-    //triggers {
-    //  cron(opts.cron)
-    //}
-    environmentVariables(TAG: TAG)
-    definition {
-      cpsScm {
-        scm {
-	  git {
-	    remote {
-	      url('git@git-server:intermittency.git')
-	      credentials('gitolite-jenkins')
-	    }
-	    branches(BRANCH_NAME)
-	    scriptPath('Jenkinsfile.entsoe')
+pipelineJob('intermittency-entsoe') {
+  triggers {
+    cron('45 */4 * * *')
+  }
+  environmentVariables(TAG: TAG)
+  definition {
+    cpsScm {
+      scm {
+	git {
+	  remote {
+	    url('git@git-server:intermittency.git')
+	    credentials('gitolite-jenkins')
 	  }
-        }
+	  branches(BRANCH_NAME)
+	  scriptPath('Jenkinsfile.entsoe')
+	}
       }
     }
   }
