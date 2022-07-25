5390789f9536bc1a370ec2198d9b2efbb82bf996
intermittent.energy <git@intermittent.energy>
2022-07-25 16:08:22 +0200

    screenshot and tweet jenkins job

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -24,3 +24,21 @@ pipelineJob("intermittency-${BRANCH_NAME}-refresh") {
     }
   }
 }
+
+pipelineJob("intermittency-${BRANCH_NAME}-tweet") {
+  environmentVariables(TAG: TAG, BRANCH_NAME: BRANCH_NAME)
+  definition {
+    cpsScm {
+      scm {
+	git {
+	  remote {
+	    url('git@git-server:intermittency.git')
+	    credentials('gitolite-jenkins')
+	  }
+	  branches(BRANCH_NAME)
+	  scriptPath('Jenkinsfile.tweet')
+	}
+      }
+    }
+  }
+}
