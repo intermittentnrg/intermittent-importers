b8e72446c4b283f400dcd9333de7a8e540cd74f3
intermittent.energy <git@intermittent.energy>
2022-08-20 18:59:04 +0200

    Make old migration irreversible

--- db/migrate/31_primary_key_prices.rb
+++ db/migrate/31_primary_key_prices.rb
@@ -1,12 +1,8 @@
 class PrimaryKeyPrices < ActiveRecord::Migration[5.1]
   def change
-    reversible do |dir|
-      dir.up do
-        #deduplicate:
-        execute "DELETE FROM prices p1 WHERE EXISTS ( SELECT * FROM prices WHERE created_at>p1.created_at AND area_id=p1.area_id AND time=p1.time)"
-        execute "DROP INDEX entsoe_prices_time_idx"
-        execute "ALTER TABLE prices ADD PRIMARY KEY (area_id,time)"
-      end
-    end
+    #deduplicate:
+    execute "DELETE FROM prices p1 WHERE EXISTS ( SELECT * FROM prices WHERE created_at>p1.created_at AND area_id=p1.area_id AND time=p1.time)"
+    execute "DROP INDEX entsoe_prices_time_idx"
+    execute "ALTER TABLE prices ADD PRIMARY KEY (area_id,time)"
   end
 end
