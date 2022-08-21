424b6f593e4678bd5d18bd9b407a67bba0853197
intermittent.energy <git@intermittent.energy>
2022-08-22 01:18:14 +0200

    Disable JIT for migration

--- db/migrate/35_generation_production_type_id_assign.rb
+++ db/migrate/35_generation_production_type_id_assign.rb
@@ -2,6 +2,7 @@ class GenerationProductionTypeIdAssign < ActiveRecord::Migration[5.2]
   def change
     reversible do |dir|
       dir.up do
+        execute "set jit = off"
         execute "UPDATE generation g SET production_type_id = pt.id FROM production_types pt WHERE g.production_type::text = pt.name"
       end
     end
