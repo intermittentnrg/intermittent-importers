0251e9934344872e2a022586fd25b80088217a87
intermittent.energy <git@intermittent.energy>
2022-09-17 15:12:25 +0200

    entsoe-price fetch prices until tomorrow

--- app/models/price.rb
+++ app/models/price.rb
@@ -8,7 +8,7 @@ class Price < ActiveRecord::Base
   def self.parsers_each(source)
     self.joins(:area).group(:'area.code').where("time > ?", 6.month.ago).where(area: {source: source.source_id}).pluck(:'area.code', Arel.sql("LAST(time, time)")).each do |country, from|
       from = from.to_datetime
-      to = [from + 1.year, DateTime.now.beginning_of_hour].min
+      to = [from + 1.year, DateTime.tomorrow.to_datetime.beginning_of_hour].min
 
       yield ENTSOE::Price.new(country: country, from: from, to: to)
     end
