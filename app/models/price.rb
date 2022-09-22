92df67a16b10e2df00f62598a6ebf8479f486f64
intermittent.energy <git@intermittent.energy>
2022-09-22 18:23:32 +0200

    Skip price fetch if already up to date

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,4 +1,3 @@
-require 'semantic_logger'
 require 'composite_primary_keys'
 
 class Price < ActiveRecord::Base
@@ -7,10 +6,22 @@ class Price < ActiveRecord::Base
 
   def self.parsers_each(source)
     self.joins(:area).group(:'area.code').where("time > ?", 6.month.ago).where(area: {source: source.source_id}).pluck(:'area.code', Arel.sql("LAST(time, time)")).each do |country, from|
-      from = from.to_datetime
-      to = [from + 1.year, DateTime.tomorrow.to_datetime.beginning_of_hour].min
+      SemanticLogger.tagged(country) do
+        from = from.to_datetime
+        to = [from + 1.year, DateTime.tomorrow.to_datetime.beginning_of_hour].min
+        if from.to_date == to
+          @@logger.warn "data is up to date"
+          next
+        end
 
-      yield ENTSOE::Price.new(country: country, from: from, to: to)
+        if source == Nordpool::Price
+          (from..to).each do |date|
+            yield source.new date
+          end
+        else
+          yield source.new(country: country, from: from, to: to)
+        end
+      end
     end
   end
 end
