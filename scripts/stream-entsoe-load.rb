eab39607c6136c9bf86fe96aabe4c45f635a5fb3
intermittent.energy <git@intermittent.energy>
2023-05-31 09:57:59 +0200

    Use logger

--- scripts/stream-entsoe-load.rb
+++ scripts/stream-entsoe-load.rb
@@ -11,7 +11,7 @@ from = ARGV.shift
 to = ARGV.shift
 
 (ARGV.present? ? ARGV : ENTSOE::DOMAIN_MAPPINGS.keys).each do |country|
-  puts country
+  logger.info country
   area_id = Area.where(source: ENTSOE::Generation.source_id, code: country).pluck(:id).first
   e = ENTSOE::Load.new country: country, from: from, to: to
   points = e.points
@@ -21,6 +21,7 @@ to = ARGV.shift
   end
   #require 'pry' ; binding.pry
   puts points
+  logger.info "#{points.length} points"
   Load.upsert_all points
 rescue
   puts $!
