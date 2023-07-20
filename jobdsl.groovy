6c7ba31cb250e0730f8be4099f46e8c9df5ef50b
intermittent.energy <git@intermittent.energy>
2023-07-20 14:59:57 +0200

    Add deep-refresh job

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -57,6 +57,37 @@ pipelineJob("intermittency-${BRANCH_NAME}/refresh") {
   }
 }
 
+pipelineJob("intermittency-${BRANCH_NAME}/deep-refresh") {
+  properties {
+    disableConcurrentBuilds()
+    pipelineTriggers {
+      triggers {
+	cron {
+	  spec('H 3 * * *')
+	}
+      }
+    }
+  }
+  logRotator {
+    numToKeep(50)
+  }
+  environmentVariables(TAG: TAG, BRANCH_NAME: BRANCH_NAME)
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('git@git-server:intermittency.git')
+            credentials('gitolite-jenkins')
+          }
+          branches(BRANCH_NAME)
+          scriptPath('Jenkinsfile.deep-refresh')
+        }
+      }
+    }
+  }
+}
+
 pipelineJob("intermittency-${BRANCH_NAME}/manual") {
   previousNames("intermittency-${BRANCH_NAME}-manual")
   parameters {
