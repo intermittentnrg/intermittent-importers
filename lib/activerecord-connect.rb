f0da9c38f206ce14a229f47615d08809452a24cf
intermittent.energy <git@intermittent.energy>
2023-08-26 23:50:07 +0200

    Enable activerecord query cache

--- lib/activerecord-connect.rb
+++ lib/activerecord-connect.rb
@@ -3,3 +3,4 @@ require 'active_record'
 
 ActiveRecord::Base.configurations = YAML.unsafe_load(ERB.new(File.read('config/database.yaml')).result)
 ActiveRecord::Base.establish_connection(:development)
+ActiveRecord::Base.connection.enable_query_cache!
