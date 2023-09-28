6356af4f12d540f0dd8c94f48cb6639d3e3eda57
intermittent.energy <git@intermittent.energy>
2023-09-28 17:43:13 +0200

    log benchmark info for aggregation queries

--- app/models/generation_unit.rb
+++ app/models/generation_unit.rb
@@ -1,22 +1,24 @@
 require 'composite_primary_keys'
 
 class GenerationUnit < ActiveRecord::Base
+  include SemanticLogger::Loggable
   self.table_name = 'generation_unit'
   #self.primary_keys = :time, :unit_id
   belongs_to :unit
 
   def self.aggregate_to_generation(from, to, where)
-    r = connection.exec_query <<-SQL
-      INSERT INTO generation (time, area_id, production_type_id, value)
-      SELECT time, u.area_id, u.production_type_id, SUM(value) AS value
-      FROM generation_unit g
-      INNER JOIN units u ON(g.unit_id=u.id)
-      INNER JOIN areas a ON(u.area_id=a.id)
-      WHERE time BETWEEN '#{from}' AND '#{to}' AND #{where}
-      GROUP BY 1,2,3
-      ON CONFLICT (area_id, production_type_id, "time") DO UPDATE set value = EXCLUDED.value
-    SQL
-    # require 'pry' ; binding.pry
+    logger.benchmark_info("aggregate_to_generation") do
+      r = connection.exec_query <<-SQL
+        INSERT INTO generation (time, area_id, production_type_id, value)
+        SELECT time, u.area_id, u.production_type_id, SUM(value) AS value
+        FROM generation_unit g
+        INNER JOIN units u ON(g.unit_id=u.id)
+        INNER JOIN areas a ON(u.area_id=a.id)
+        WHERE time BETWEEN '#{from}' AND '#{to}' AND #{where}
+        GROUP BY 1,2,3
+        ON CONFLICT (area_id, production_type_id, "time") DO UPDATE set value = EXCLUDED.value
+      SQL
+    end
 
     Generation.aggregate_to_capture(from, to, where)
   end
