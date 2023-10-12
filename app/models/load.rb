5fecc30824d10f8e045ec8e30e65d5a436992a29
intermittent.energy <git@intermittent.energy>
2023-10-12 20:38:28 +0200

    Load.inspect

--- app/models/load.rb
+++ app/models/load.rb
@@ -3,4 +3,15 @@ require 'composite_primary_keys'
 class Load < ActiveRecord::Base
   self.table_name = 'load'
   belongs_to :area
+
+  def inspect
+    if value > 1000000
+      s = "#{value/1000000.0} GW"
+    elsif value > 1000
+      s = "#{value/1000.0} MW"
+    else
+      s = "#{value} kW"
+    end
+    "#<Load area_id: #{area_id} time: #{time} value: #{s}>"
+  end
 end
