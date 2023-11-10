cc96e5fd583f694ff3a4b656c011009dd1de4ea1
intermittent.energy <git@intermittent.energy>
2023-11-10 04:02:29 +0100

    generation view apt_id

---
+++ db/migrate/20231110025946_generation_view_apt.rb
@@ -0,0 +1,10 @@
+class GenerationViewApt < ActiveRecord::Migration[7.0]
+  def change
+    execute <<-SQL
+    CREATE OR REPLACE VIEW generation AS
+      SELECT area_id, production_type_id, time, value, areas_production_type_id
+      FROM generation_data g
+      INNER JOIN areas_production_types apt ON(areas_production_type_id=apt.id)
+    SQL
+  end
+end
