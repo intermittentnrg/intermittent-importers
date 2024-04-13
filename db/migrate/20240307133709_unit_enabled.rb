85918752882f9944412b37e0715f3034f48a9486
intermittent.energy <git@intermittent.energy>
2024-04-13 17:19:35 +0200

    Tidy units.enabled migration

--- db/migrate/20240307133709_unit_enabled.rb
+++ db/migrate/20240307133709_unit_enabled.rb
@@ -1,7 +1,7 @@
 class UnitEnabled < ActiveRecord::Migration[7.0]
   def change
     change_table :units do |t|
-      t.change :enabled, :boolean, null: false, default: true
+      t.boolean :enabled, null: false, default: true
     end
   end
 end
