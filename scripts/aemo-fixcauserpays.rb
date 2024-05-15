6f98a377c26f650aba3e4940641a244269ac7a62
intermittent.energy <git@intermittent.energy>
2024-05-16 01:38:45 +0200

    fixcauserpays delete duplicate rows

--- scripts/aemo-fixcauserpays.rb
+++ scripts/aemo-fixcauserpays.rb
@@ -12,54 +12,83 @@ _to=Chronic.parse(ARGV.shift).to_date
 # Disable automatic compression job
 conn.execute "SELECT alter_job((SELECT job_id FROM timescaledb_information.jobs WHERE proc_name='policy_compression' AND hypertable_name = 'generation_unit_hires'), scheduled => false)"
 
+# Let UPDATE in progress finish on ctrl-c
 $lock = false
 $exiting = false
 trap("SIGINT") do
-  throw :ctrl_c if !$lock || $exiting
+  abort if !$lock || $exiting
+  $stderr.puts "Exiting after chunk finishes"
   $exiting = true
 end
-chunks = []
-(_from.._to).each do |from|
+
+(_from..._to).each do |from|
   to = from + 1.day
 
   # Select chunks for the current date range
   chunks = conn.select_values <<~SQL
-    SELECT g.tableoid::regclass FROM generation_unit_hires g
-    INNER JOIN units u ON(unit_id=u.id)
-    INNER JOIN units uh ON(u.internal_id=uh.hires_internal_id)
+    SELECT gbad.tableoid::regclass FROM generation_unit_hires gbad
+    INNER JOIN units ubad ON(unit_id=ubad.id)
+    INNER JOIN units ugood ON(ubad.internal_id=ugood.hires_internal_id)
     WHERE
-      u.area_id=338 AND
+      ubad.area_id=338 AND
+      ubad.hires_internal_id IS NULL AND
       time >= '#{from}' AND time < '#{to}'
-    GROUP BY tableoid;
+    GROUP BY tableoid
   SQL
   next if chunks.empty?
 
   # Decompress chunks
   chunks.each do |chunk|
-    conn.execute "SELECT decompress_chunk('#{chunk}', true);"
+    conn.execute "SELECT decompress_chunk('#{chunk}', true)"
   rescue ActiveRecord::Deadlocked
     puts $!.inspect
     retry
   end
 
+  # Update data
   $lock = true
-  conn.execute <<~SQL
-    UPDATE generation_unit_hires g
-    SET unit_id=uh.id
-    FROM
-      units u,
-      units uh
-    WHERE
-      unit_id=u.id AND
-      u.internal_id=uh.hires_internal_id AND
-      u.area_id=338 AND
-      time >= '#{from}' AND time < '#{to}'
-  SQL
+  begin
+    conn.execute <<~SQL
+      UPDATE generation_unit_hires gbad
+      SET unit_id=ugood.id
+      FROM
+        units ubad,
+        units ugood
+      WHERE
+        gbad.unit_id=ubad.id AND
+        ubad.internal_id=ugood.hires_internal_id AND
+        ubad.area_id=338 AND
+        ubad.hires_internal_id IS NULL AND
+        time >= '#{from}' AND time < '#{to}'
+    SQL
+  rescue ActiveRecord::RecordNotUnique
+    puts $!.inspect
+    conn.execute <<~SQL
+      DELETE FROM
+        generation_unit_hires gbad
+      USING
+        units ubad,
+        units ugood,
+        generation_unit_hires ggood
+      WHERE
+        gbad.unit_id=ubad.id AND
+        ubad.hires_internal_id IS NULL AND
+        ubad.area_id=338 AND
+        ubad.internal_id=ugood.hires_internal_id AND
+        ugood.id=ggood.unit_id AND
+        gbad.time = ggood.time AND
+        gbad.time >= '#{from}' AND gbad.time < '#{to}' AND
+        ggood.time >= '#{from}' AND ggood.time < '#{to}' AND
+        gbad.value = ggood.value
+    SQL
+    retry
+  end
 
   # Compress chunks
   chunks.each do |chunk|
-    conn.execute "SELECT compress_chunk('#{chunk}', true);"
+    conn.execute "SELECT compress_chunk('#{chunk}', true)"
   end
+  break if $exiting
   $lock = false
 end
 #binding.irb
