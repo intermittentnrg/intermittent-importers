001a0c67a896ef5ceaa8df8b4f73a15040a26927
intermittent.energy <git@intermittent.energy>
2024-05-17 02:06:58 +0200

    Update stations.json from OpenNem

--- scripts/aemo-units-metadata.rb
+++ scripts/aemo-units-metadata.rb
@@ -2,11 +2,13 @@
 # coding: utf-8
 require './lib/init'
 require './lib/activerecord-connect'
+require 'fast_jsonparser'
 
 #url = "https://raw.githubusercontent.com/opennem/opennem/master/opennem/data/stations.json"
 #response = Faraday.get(url)
 json = FastJsonparser.load 'data/aemo/stations.json'
 #require 'pry' ; binding.pry
+logger = SemanticLogger[$0]
 
 json.each do |facility|
   facility[:facilities].each do |f2|
@@ -15,14 +17,19 @@ json.each do |facility|
       unit.name = "#{facility[:name]} #{f2[:code]}"
       production_type = Opennem::Base::FUEL_MAP[f2[:fueltech_id]]
       if ["hydro_pumped_storage","battery_charging"].include? production_type
-        if production_type != unit.production_type
-          raise "New #{production_type} unit, generation data should be inverted"
+        if production_type != unit.production_type.name
+          logger.error "New #{production_type} unit #{unit.id} #{unit.name}, old #{unit.production_type.name}, generation data needs to be inverted"
+          #next
         end
       end
       unit.production_type = ProductionType.find_by! name: production_type
       unit.area = Area.find_by(code: f2[:network_region], source: Aemo::Base.source_id)
-      puts unit.name if unit.changed?
-      unit.save!
+      if unit.changed?
+        puts unit.name
+        puts unit.changes.inspect
+        #binding.irb
+      end
+      #unit.save!
       #puts unit.inspect
     else
       #puts "No unit #{facility[:code]}"
