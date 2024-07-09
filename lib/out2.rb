ed9ff70dc5ba19bebe45ac564ed6648824dc64cb
intermittent.energy <git@intermittent.energy>
2024-07-10 01:20:38 +0200

    Add Taipower

--- lib/out2.rb
+++ lib/out2.rb
@@ -76,9 +76,32 @@ module Out2
   class Unit < Base
     include SemanticLogger::Loggable
 
+    @@units = {}
     def self.run(data, from, to, source_id)
       updated_rows = nil
       if data.present?
+
+        data.each do |p|
+          if p[:country] && p[:unit] && p[:production_type]
+            k = [source_id, p[:production_type], p[:unit]]
+            p[:unit_id] = (@@units[k] ||= ::Unit.joins(:production_type, :area).where(internal_id: p[:unit], area: {source: source_id, code: p[:country]}, production_type: {name: p[:production_type]}).pluck(:id).first)
+            unless p[:unit_id]
+              require 'pry' ; binding.pry
+              logger.warn("creating unit for #{p[:country]}/#{p[:production_type]}/#{p[:unit]}")
+              area_id = ::Area.where(source: source_id, code: p[:country]).pluck(:id).first
+              pt_id = ::ProductionType.where(name: p[:production_type]).pluck(:id).first
+              raise p.inspect unless area_id
+              raise p.inspect unless pt_id
+              unit = ::Unit.create!(area_id:, production_type_id: pt_id, internal_id: p[:unit])
+              @@units[k] = p[:unit_id] = unit.id
+            end
+            p.delete :country
+            p.delete :unit
+            p.delete :production_type
+          end
+        end
+        #require 'pry' ; binding.pry
+
         start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
         if data.length > 100_000
           conn = ActiveRecord::Base.connection
