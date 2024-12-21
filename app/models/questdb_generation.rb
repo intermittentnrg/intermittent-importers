aac71768a24cd007e49e793a7beaf9733fb77396
intermittent.energy <git@intermittent.energy>
2024-12-21 11:25:03 +0100

    Cleanup old files

--- app/models/questdb_generation.rb
+++
@@ -1,3 +0,0 @@
-class QuestdbGeneration < ActiveRecord::Base
-  establish_connection :questdb
-end
--- lib/aemo_wem_archive.rb
+++ lib/aemo_wem_archive.rb
@@ -1,4 +1,4 @@
-module AemoArchiveWem
+module AemoWemArchive
   class ScadaArchive
     URL = "https://data.wa.aemo.com.au/datafiles/facility-scada/"
     TARGET = AemoWem::Scada
