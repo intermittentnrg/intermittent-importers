144f91d4a76958963efae3451ddb1c0418659463
intermittent.energy <git@intermittent.energy>
2025-01-20 13:41:29 +0100

    pricemap: post to bluesky

--- scripts/grafanimate-upload-bsky.rb
+++ scripts/grafanimate-upload-bsky.rb
@@ -35,7 +35,6 @@ resp = sky.get_request(
 
 
 ### UPLOAD
-
 video_path = "render.mp4"
 begin
   job = sky_video.post_request(
@@ -60,7 +59,6 @@ end until status['jobStatus']['blob']
 
 
 ### POST
-
 text = <<-EOF
 Day ahead spot prices tomorrow #{Date.tomorrow.strftime('%a %b %-d')} UTC
 Range capped at €300/MWh.
