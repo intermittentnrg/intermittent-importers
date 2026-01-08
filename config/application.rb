0dfe982010132ad26914054c7c0a826c332fe31c
intermittent.energy <git@intermittent.energy>
2026-01-08 15:02:04 +0100

    Fix asset serving

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module Intermittency
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.schema_format = :sql
+
+    config.assets.enabled = true
   end
 end
