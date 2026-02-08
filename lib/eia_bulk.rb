333c549ce8e2d1868d3b9af042e274cfca2c1be1
intermittent.energy <git@intermittent.energy>
2026-02-08 07:35:50 +0100

    eia_bulk_spec.rb

--- lib/eia_bulk.rb
+++ lib/eia_bulk.rb
@@ -11,52 +11,63 @@ module EiaBulk
     end
 
     def self.cli(args)
-      if args.empty?
-        warn "#{$PROGRAM_NAME}: <file>"
+      if args.empty? || args.length > 2
+        warn "#{$PROGRAM_NAME}: <file> [filter]"
         return
       end
 
-      new(*args).process
+      new.add_file(args[0], args[1]).done!
     end
 
-    def initialize(path, filter = nil)
-      if path =~ /\.zip$/i
-        zip_file = Zip::File.open(path)
-        @file = zip_file.first.get_input_stream
-      else
-        @file = File.open(path, 'r')
-      end
-      @filter = filter
+    def add_file(path, filter = nil)
+      setup_database
 
-      super()
+      file = if path =~ /\.zip$/i
+               zip_file = Zip::File.open(path)
+               zip_file.first.get_input_stream
+             else
+               File.open(path, 'r')
+             end
 
-      ActiveRecord::Base.connection.create_enum :eia_bulk_area, Area.where(source: 'eia').order(:code).pluck(:code)
-    end
-
-    def parse_time(t)
-      "#{t[0, 4]}-#{t[4, 2]}-#{t[6, 5]}:00Z"
-    end
-
-    def process
       conn = ActiveRecord::Base.connection.raw_connection
       enco = PG::TextEncoder::CopyRow.new
       conn.copy_data "COPY #{self.class::BULK_TABLE} FROM STDIN", enco do
-        @file.each_line do |line|
+        file.each_line do |line|
           next if line[0] == '\r'
 
           m = line.match(/"series_id":"(.*?)"/)
           next unless m
 
-          @series = m[1]
-          next if @filter && @filter && !@series.include?(@filter)
+          series = m[1]
+          next if filter && !series.include?(filter)
 
-          process_line(line) { |row| conn.put_copy_data(row) }
+          process_line(series, line) { |row| conn.put_copy_data(row) }
         end
       end
-      self.class.copy
+      self.class.copy_data_from_temp_table
+
+      self
+    ensure
+      file&.close
+      cleanup_database
     end
 
-    def self.copy
+    def setup_database
+      ActiveRecord::Base.connection.create_enum :eia_bulk_area, Area.where(source: 'eia').order(:code).pluck(:code)
+    end
+
+    def cleanup_database
+      ActiveRecord::Base.connection.drop_table self.class::BULK_TABLE, if_exists: true
+      ActiveRecord::Base.connection.drop_enum :eia_bulk_area
+    end
+
+    def done! ; end
+
+    def parse_time(t)
+      "#{t[0, 4]}-#{t[4, 2]}-#{t[6, 5]}:00Z"
+    end
+
+    def self.copy_data_from_temp_table
       self::TARGET_MODEL.disable_compression_policy!
       self::TARGET_MODEL.chunks.each do |chunk|
         logger.benchmark_info("decompress chunk #{chunk.chunk_name}") do
@@ -69,24 +80,18 @@ module EiaBulk
         end
       end
       self::TARGET_MODEL.enable_compression_policy!
-      ActiveRecord::Base.connection.drop_table self::BULK_TABLE
-      copy_cleanup
-    end
-
-    def self.copy_cleanup
-      ActiveRecord::Base.connection.drop_enum :eia_bulk_area
     end
   end
 
   class Generation < Base
     include SemanticLogger::Loggable
 
-    V_USA = Validate::RULES['eia']
+    V_USA = Eia::Generation::VALIDATION_GEN
     V_USA_ALL = V_USA['all']
     BULK_TABLE = 'eia_bulk_generation'
     TARGET_MODEL = ::Generation
 
-    def initialize(*args)
+    def setup_database
       super
       ActiveRecord::Base.connection.create_enum :eia_bulk_production_type,
                                                 %w[fossil_gas fossil_hard_coal fossil_oil hydro nuclear other solar
@@ -99,21 +104,27 @@ module EiaBulk
       end
     end
 
-    def process_line(line)
-      series = @series.split(/\./)
+    def cleanup_database
+      super
+      ActiveRecord::Base.connection.drop_enum :eia_bulk_production_type
+    end
+
+    def process_line(series, line)
+      # series is the full series_id string (e.g., "EBA.CISO-ALL.NG.SUN.H")
+      parts = series.split(/\./)
       # 0   1        2  3   4
       # EBA.US48-ALL.NG.H
       # EBA.CISO-ALL.NG.SUN.H
-      return unless series.length == 5 # skip generation without fuel type
-      return unless series[2] == 'NG' # net generation
-      return unless series[4] == 'H' # timezone
+      return unless parts.length == 5 # skip generation without fuel type
+      return unless parts[2] == 'NG' # net generation
+      return unless parts[4] == 'H' # timezone
 
-      logger.benchmark_info "series #{@series}" do
+      logger.benchmark_info "series #{series}" do
         json = FastJsonparser.parse(line, symbolize_keys: false)
 
-        country = series[1].split(/-/)[0]
+        country = parts[1].split(/-/)[0]
 
-        production_type = Eia::Base::FUEL_MAP[series[3]]
+        production_type = Eia::Base::FUEL_MAP[parts[3]]
 
         json['data'].each do |p|
           next nil unless p[1]
@@ -142,22 +153,17 @@ module EiaBulk
         ON CONFLICT ON CONSTRAINT generation_pkey DO UPDATE SET value = EXCLUDED.value WHERE generation_data.value<>EXCLUDED.value
       SQL
     end
-
-    def self.copy_cleanup
-      ActiveRecord::Base.connection.drop_enum :eia_bulk_production_type
-      super
-    end
   end
 
   class Demand < Base
     include SemanticLogger::Loggable
 
-    V_USA = Validate::RULES['eia']
+    V_USA = Eia::Load::VALIDATION_LOAD
     V_USA_ALL = V_USA['all']
     BULK_TABLE = 'eia_bulk_demand'
     TARGET_MODEL = Load
 
-    def initialize(*args)
+    def setup_database
       super
       ActiveRecord::Base.connection.create_table BULK_TABLE, id: false, temporary: true do |t|
         t.timestamptz :time, null: false
@@ -166,17 +172,18 @@ module EiaBulk
       end
     end
 
-    def process_line(line)
+    def process_line(series, line)
+      # series is the full series_id string (e.g., "EBA.CISO-ALL.D.H")
       # 0   1      2 3
       # EBA.SW-ALL.D.H
-      series = @series.split(/\./)
-      return unless series[2] == 'D'
-      return unless series[3] == 'H'
+      parts = series.split(/\./)
+      return unless parts[2] == 'D'
+      return unless parts[3] == 'H'
 
-      country, country_suffix = series[1].split(/-/)
+      country, country_suffix = parts[1].split(/-/)
       return if country_suffix != 'ALL'
 
-      logger.benchmark_info "series #{@series}" do
+      logger.benchmark_info "series #{series}" do
         json = FastJsonparser.parse(line, symbolize_keys: false)
         json['data'].each do |p|
           next nil unless p[1]
@@ -212,7 +219,7 @@ module EiaBulk
     BULK_TABLE = 'eia_bulk_interchange'
     TARGET_MODEL = Transmission
 
-    def initialize(*args)
+    def setup_database
       super
       ActiveRecord::Base.connection.create_table BULK_TABLE, id: false, temporary: true do |t|
         t.timestamptz :time, null: false
@@ -222,20 +229,19 @@ module EiaBulk
       end
     end
 
-    def process_line(line)
+    def process_line(series, line)
+      # series is the full series_id string (e.g., "EBA.CISO-AZPS.ID.H")
       # 0   1         2  3
       # EBA.CISO-AZPS.ID.H
-      series = @series.split(/\./)
-      return unless series[2] == 'ID'
-      return unless series[3] == 'H'
-      raise series.inspect unless series.length == 4
+      parts = series.split(/\./)
+      return unless parts[2] == 'ID'
+      return unless parts[3] == 'H'
+      raise series.inspect unless parts.length == 4
 
-      from_area, to_area = series[1].split(/-/)
+      from_area, to_area = parts[1].split(/-/)
 
-      logger.benchmark_info "series #{@series}" do
+      logger.benchmark_info "series #{series}" do
         json = FastJsonparser.parse(line, symbolize_keys: false)
-
-        # from, to = parse_from_to(json)
 
         json['data'].each do |p|
           next nil unless p[1]
---
+++ spec/lib/eia_bulk_spec.rb
@@ -0,0 +1,23 @@
+require './spec/spec_helper'
+
+RSpec.describe 'EiaBulk' do
+  let(:sample_txt_path) { './spec/fixtures/eia_bulk_sample.txt' }
+
+  describe 'Generation.cli' do
+    it 'processes generation data from file' do
+      EiaBulk::Generation.cli([sample_txt_path, 'CISO'])
+    end
+  end
+
+  describe 'Demand.cli' do
+    it 'processes demand data from file' do
+      EiaBulk::Demand.cli([sample_txt_path, 'CISO'])
+    end
+  end
+
+  describe 'Interchange.cli' do
+    it 'processes interchange data from file' do
+      EiaBulk::Interchange.cli([sample_txt_path, 'CISO'])
+    end
+  end
+end
