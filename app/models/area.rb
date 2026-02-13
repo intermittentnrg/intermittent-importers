8b96b2e189207ae4737c4507b0e71bb462fbdb02
intermittent.energy <git@intermittent.energy>
2026-02-13 15:16:49 +0100

    Area.location is optional

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,6 +1,6 @@
 class Area < ActiveRecord::Base
   self.inheritance_column = nil
-  belongs_to :location
+  belongs_to :location, optional: true
   has_many :load
   has_many :units
   has_many :prices
